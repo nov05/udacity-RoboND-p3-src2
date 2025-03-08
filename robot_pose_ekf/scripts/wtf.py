f9d57821ff4602881c8c63e0a9c94188217ff685
nov05 <215590+Nov05@users.noreply.github.com>
2025-03-08 17:37:21 -0600

    .

--- robot_pose_ekf/scripts/wtf.py
+++ robot_pose_ekf/scripts/wtf.py
@@ -3,9 +3,12 @@
 import rospy
 from robot_pose_ekf.srv import GetStatus, GetStatusRequest
 
+
+
 if __name__ == '__main__':
+
     rospy.init_node('spawner', anonymous=True)
-    print 'looking for node robot_pose_ekf...'
+    print 'Looking for node robot_pose_ekf...'
     rospy.wait_for_service('robot_pose_ekf/get_status')
 
     s = rospy.ServiceProxy('robot_pose_ekf/get_status', GetStatus)
