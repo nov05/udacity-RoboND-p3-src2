5aa6099fd2126d229488beca46ead9372ea9cdbc
nov05 <215590+Nov05@users.noreply.github.com>
2025-03-08 20:02:15 -0600

    .

--- turtlebot3_simulations/turtlebot3_gazebo/include/turtlebot3_gazebo/turtlebot3_drive.h
+++ turtlebot3_simulations/turtlebot3_gazebo/include/turtlebot3_gazebo/turtlebot3_drive.h
@@ -1,18 +1,18 @@
 /*******************************************************************************
-* Copyright 2016 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright 2016 ROBOTIS CO., LTD.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 /* Authors: Taehun Lim (Darby) */
 
@@ -29,54 +29,54 @@
 #define RAD2DEG (180.0 / M_PI)
 
 #define CENTER 0
-#define LEFT   1
-#define RIGHT  2
+#define LEFT 1
+#define RIGHT 2
 
-#define LINEAR_VELOCITY  0.3
+#define LINEAR_VELOCITY 0.3
 #define ANGULAR_VELOCITY 1.5
 
 #define GET_TB3_DIRECTION 0
 #define TB3_DRIVE_FORWARD 1
-#define TB3_RIGHT_TURN    2
-#define TB3_LEFT_TURN     3
+#define TB3_RIGHT_TURN 2
+#define TB3_LEFT_TURN 3
 
 class Turtlebot3Drive
 {
- public:
-  Turtlebot3Drive();
-  ~Turtlebot3Drive();
-  bool init();
-  bool controlLoop();
+public:
+    Turtlebot3Drive();
+    ~Turtlebot3Drive();
+    bool init();
+    bool controlLoop();
 
- private:
-  // ROS NodeHandle
-  ros::NodeHandle nh_;
-  ros::NodeHandle nh_priv_;
+private:
+    // ROS NodeHandle
+    ros::NodeHandle nh_;
+    ros::NodeHandle nh_priv_;
 
-  // ROS Parameters
+    // ROS Parameters
 
-  // ROS Time
+    // ROS Time
 
-  // ROS Topic Publishers
-  ros::Publisher cmd_vel_pub_;
+    // ROS Topic Publishers
+    ros::Publisher cmd_vel_pub_;
 
-  // ROS Topic Subscribers
-  ros::Subscriber laser_scan_sub_;
-  ros::Subscriber odom_sub_;
+    // ROS Topic Subscribers
+    ros::Subscriber laser_scan_sub_;
+    ros::Subscriber odom_sub_;
 
-  // Variables
-  double escape_range_;
-  double check_forward_dist_;
-  double check_side_dist_;
+    // Variables
+    double escape_range_;
+    double check_forward_dist_;
+    double check_side_dist_;
 
-  double scan_data_[3] = {0.0, 0.0, 0.0};
+    double scan_data_[3] = {0.0, 0.0, 0.0};
 
-  double tb3_pose_;
-  double prev_tb3_pose_;
+    double tb3_pose_;
+    double prev_tb3_pose_;
 
-  // Function prototypes
-  void updatecommandVelocity(double linear, double angular);
-  void laserScanMsgCallBack(const sensor_msgs::LaserScan::ConstPtr &msg);
-  void odomMsgCallBack(const nav_msgs::Odometry::ConstPtr &msg);
+    // Function prototypes
+    void updatecommandVelocity(double linear, double angular);
+    void laserScanMsgCallBack(const sensor_msgs::LaserScan::ConstPtr &msg);
+    void odomMsgCallBack(const nav_msgs::Odometry::ConstPtr &msg);
 };
 #endif // TURTLEBOT3_DRIVE_H_
