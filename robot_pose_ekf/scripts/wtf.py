73b39e5411b1e6b45247343c8f2505e9de743e58
nov05 <215590+Nov05@users.noreply.github.com>
2025-03-08 23:15:07 -0600

    robot_pose_ekl python3

--- robot_pose_ekf/scripts/wtf.py
+++ robot_pose_ekf/scripts/wtf.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import rospy
 from robot_pose_ekf.srv import GetStatus, GetStatusRequest
 
 if __name__ == '__main__':
 
     rospy.init_node('spawner', anonymous=True)
-    print 'Looking for node robot_pose_ekf...'
+    # print 'Looking for node robot_pose_ekf...' 
+    print("Looking for node \"robot_pose_ekf\"...")
     rospy.wait_for_service('robot_pose_ekf/get_status')
 
     s = rospy.ServiceProxy('robot_pose_ekf/get_status', GetStatus)
     resp = s.call(GetStatusRequest())
-    print resp.status
+    # print resp.status
+    print(resp.status)
